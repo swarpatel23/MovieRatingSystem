7fb7baee360286ee30fb0ca77b4b343289afdeba
swarpatel23 <smp1613@gmail.com>
2020-04-26 23:35:00 +0530

    dockerfile added

--- review_model/training/api.py
+++ review_model/training/api.py
@@ -1,34 +1,33 @@
 from flask import Flask, request, jsonify
 from pickle import load
 import traceback
-import pandas as pd
-import numpy as np
 
 # Your API definition
 app = Flask(__name__)
+
 
 @app.route('/model/predict', methods=['POST'])
 def predict():
     if lr:
         try:
-            
+
             json_ = request.json
             print(json_["review"].lower())
-           
-           
-            #return "done"
+
+            # return "done"
             return str(lr.predict(vect.transform([json_["review"]])))
 
         except:
 
             return jsonify({'trace': traceback.format_exc()})
     else:
-        print ('Train the model first')
+        print('Train the model first')
         return ('No model here to use')
+
 
 if __name__ == '__main__':
     try:
-        port = int(sys.argv[1]) 
+        port = int(sys.argv[1])
     except:
         port = 12345
 
